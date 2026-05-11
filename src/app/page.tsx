35f4f5eff1d7c60d7c324188d14b0136d555a391
perez <perezalexandre430@gmail.com>
2026-05-11 09:42:15 +0200

    arborescence de base

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Hero from "../components/sections/Hero";
+
+export default function Home() {
+  return (
+    <div >
+      <main >
+        <Hero />
+
+      </main>
+    </div>
+  );
+}
---
+++ src/components/sections/Hero.tsx
@@ -0,0 +1,12 @@
+export default function Hero() {
+  return (
+    <div>
+      <main>
+        <h1>Recolteo</h1>
+        <p>
+          A modern, open-source, self-hosted, and privacy-focused RSS reader.
+        </p>
+      </main>
+    </div>
+  );
+}
